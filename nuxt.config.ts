7dafe34a7542921f6c05daf0795e2fc6161c35bd
Volkov Nikolay <volkovnd@yandex.ru>
2025-08-02 12:47:38 +0300

    feat: Added formaters, linters and etc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,56 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true, telemetry: false },
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'ru' },
+      meta: [
+        {
+          name: 'google',
+          content: 'notranslate',
+        },
+        {
+          name: 'format-detection',
+          content: 'telephone=no',
+        },
+      ],
+      link: [
+        {
+          type: 'image/x-icon',
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+      ],
+    },
+  },
+  compatibilityDate: '2025-07-15',
+  telemetry: { enabled: false },
+  eslint: {
+    config: {
+      nuxt: { sortConfigKeys: true },
+      stylistic: true,
+    },
+  },
+
+  fonts: {
+    families: [{
+      name: 'Roboto',
+      display: 'swap',
+      provider: 'google',
+
+    }],
+    defaults: {
+      subsets: ['cyrillic', 'latin'],
+      preload: true,
+      weights: [400, 500, 700],
+    },
+  },
 })
